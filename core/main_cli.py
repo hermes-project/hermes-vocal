e5bd0b0fda72bcba95290b7b5d5bfe328253befb
remi.dulong <remi.dulong@telecom-sudparis.eu>
2017-07-05 20:00:27 +0200

    Structure

---
+++ core/main_cli.py
@@ -0,0 +1,11 @@
+# Command-line Interface
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+print("######################")
+print("#    Hermes-vocal    #")
+print("######################")
+
+while(42):
+    order = input("Order : ")
