e552ab3ac26820dad1f2f8e27f0a8b5d174fc7ab
LBT76240 <trailin_88_@hotmail.fr>
2017-07-07 17:18:50 +0200

    TTS STT OK

--- test_TTS.py
+++ test_TTS.py
@@ -1,28 +1,14 @@
+from gtts import gTTS
 import os
-from pocketsphinx import LiveSpeech, get_model_path
 
-model_path = get_model_path()
 
-print ("Model path : "+model_path)
 
-speech = LiveSpeech(
-	verbose=True,
-	sampling_rate=16000,
-    buffer_size=2048,
-    no_search=False,
-    full_utt=False,
-    hmm=os.path.join(model_path, 'fr-fr'),
-    lm=os.path.join(model_path, 'fr-small.lm'),
-    dic=os.path.join(model_path, 'fr.dict')
-)
 
-for phrase in speech:
-    print(phrase)
+while(True) :
+    order = input("Order : ")
+    tts = gTTS(text=order, lang='fr')
+    tts.save("good.mp3")
+    os.system("mpg321 good.mp3")
 
-#from pocketsphinx import LiveSpeech
 
-#hmdir = "/usr/share/pocketsphinx/model/fr/fr"
-#lmd = "/usr/share/pocketsphinx/model/fr/fr-small.lm"
-#dictd = "/usr/share/pocketsphinx/model/fr/fr.dict"
 
-#for phrase in LiveSpeech(): print(phrase)
