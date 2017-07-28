4fadfc3efbee67a4efc26cbc3fd9231d2da5be15
LBT76240 <trailin_88_@hotmail.fr>
2017-07-28 16:59:08 +0200

    correction bug when empty order by client

--- main_client.py
+++ main_client.py
@@ -6,10 +6,11 @@ port = 15555
 while True:
 
     request = input("Ordre : ")
-    socket=sck.socket(sck.AF_INET, sck.SOCK_STREAM)
-    socket.connect((hote, port))
-    print("Connecté à "+hote)
-    socket.send(str.encode(request))
-    ret = socket.recv(1024).decode()
-    print(ret)
-    socket.close()
+    if(request != "") :
+        socket=sck.socket(sck.AF_INET, sck.SOCK_STREAM)
+        socket.connect((hote, port))
+        print("Connecté à "+hote)
+        socket.send(str.encode(request))
+        ret = socket.recv(1024).decode()
+        print(ret)
+        socket.close()
