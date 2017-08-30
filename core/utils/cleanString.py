eb1a1d0c6ef5080b2f7624fc61ef946a88052c9c
remi.dulong <remi.dulong@telecom-sudparis.eu>
2017-08-30 14:34:27 +0100

    correction bug espace laissé par le clean de ponctuation

--- core/utils/cleanString.py
+++ core/utils/cleanString.py
@@ -2,7 +2,7 @@
 # (simplifications des caractères spéciaux)
 
 def cleanString(order):
-    order = order.strip('?!').replace("-", " ").replace("'", " ").lower()
+    order = order.strip('?!').replace("-", " ").replace("'", " ").lstrip().rstrip().lower()
     order = order.replace("â", "a").replace("ê", "e").replace("é", "e").replace("è", "e").replace("ù", "u")
 
     return order
